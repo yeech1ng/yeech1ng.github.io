282190cf2f6c0d0fdf61ad96d4b615c3bd3502e4
yeech1ng <e0968278@u.nus.edu>
2024-03-29 00:12:26 +0800

    changes

--- personal_app/app.R
+++ personal_app/app.R
@@ -1,20 +1,34 @@
+# Load necessary library
 library(shiny)
 
-# Define UI for application that draws a histogram
+# Define UI
 ui <- fluidPage(
-  sidebarLayout(
-    sidebarPanel(sliderInput("samplesize","Sample Size:",min = 100,max = 10000,value = 1000)),
-    mainPanel(plotOutput("distPlot"))
-  )
+  # Add dropdown menu for OTT media services
+  selectInput("ott_service", "Select OTT Service:",
+              choices = c("Netflix", "YouTube", "Amazon Prime", "Disney+", "YouTube Premium", "iQiyi")),
+  
+  # Add placeholder for displaying information
+  textOutput("ott_info")
 )
 
-# Define server logic required to draw a histogram
+# Define server logic
 server <- function(input, output) {
-  output$distPlot <- renderPlot({
-    hist(rnorm(input$samplesize),col='darkorchid',xlab="Sample",main="Standard Normally Distributed Sample")},
-    height=300
-  )
+  # Define reactive expression to filter data based on OTT service
+  filtered_data <- reactive({
+    switch(input$ott_service,
+           "Netflix" = 69.90,
+           "YouTube" = 62.60,
+           "Amazon Prime" = 29.30,
+           "Disney+" = 23.20,
+           "YouTube Premium" = 18.10,
+           "iQiyi" = 15.20)
+  })
+  
+  # Display information based on selected OTT service
+  output$ott_info <- renderText({
+    paste("If you use", input$ott_service, "you would be among", filtered_data(), "% of global audiences.")
+  })
 }
 
-# Run the application 
-shinyApp(ui = ui, server = server)
+# Run the Shiny app
+shinyApp(ui, server)
