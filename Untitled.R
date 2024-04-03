78520194ddb857344e5e99429dc244377542d516
yeech1ng <e0968278@u.nus.edu>
2024-04-03 12:32:29 +0800

    changes

--- Untitled.R
+++
@@ -1,5 +0,0 @@
-install.packages('rsconnect')
-
-rsconnect::setAccountInfo(name='yeech1ng',
-                          token='420586B6A6B0757A29D3D98D4613FF89',
-                          secret='BscuRuFgDCxPCX51BC2/SLyo5OhsPSjsUGiu9sbn')
