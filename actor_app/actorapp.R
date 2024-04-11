8845783ed1b5a1df95193f2ad8a750f21c31f321
yeech1ng <e0968278@u.nus.edu>
2024-04-11 17:01:48 +0800

    changes

---
+++ actor_app/actorapp.R
@@ -0,0 +1,57 @@
+library(shiny)
+library(dplyr)
+library(plotly)
+library(ggplot2)
+
+# UI definition
+ui <- fluidPage(
+  titlePanel("Correlation Plots"),
+  sidebarLayout(
+    sidebarPanel(
+      selectInput("plot_type", "Select Plot Type:",
+                  choices = c("Korean" = "kor", "Thai" = "tha", "Japanese" = "jap"))
+    ),
+    mainPanel(
+      plotlyOutput("correlation_plot")
+    )
+  )
+)
+
+# Server logic
+server <- function(input, output) {
+  # List files in the 'actor_app' directory
+  observe({
+    files <- list.files("actor_app")
+    print(files)
+  })
+  
+  # Read data from CSV files
+  kreviews <- read.csv("kor_user_reviews.csv")
+  treviews <- read.csv("tha_user_reviews.csv")
+  jreviews <- read.csv("jap_user_reviews.csv")
+  
+  # Function to create correlation plot
+  create_plot <- function(data, language) {
+    ggplot(data, aes(x = acting_cast, y = overall, color = language)) +
+      geom_jitter() +
+      geom_smooth() +
+      ggtitle(paste("Correlation between Acting Cast Ratings and Overall Ratings for", language, "dramas")) +
+      xlab("Acting Cast Ratings") +
+      ylab("Overall Ratings")
+  }
+  
+  # Render the correlation plot based on selected plot type
+  output$correlation_plot <- renderPlotly({
+    if (input$plot_type == "kor") {
+      plot <- create_plot(kreviews, "Korean")
+    } else if (input$plot_type == "tha") {
+      plot <- create_plot(treviews, "Thai")
+    } else if (input$plot_type == "jap") {
+      plot <- create_plot(jreviews, "Japanese")
+    }
+    ggplotly(plot)
+  })
+}
+
+# Run Shiny app
+shinyApp(ui = ui, server = server)
